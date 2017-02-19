3604aedb358e5c5112a9bc86f0d5a441ea2b463d
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2017-02-19 14:51:13 +0300

    Add base language primitives
     - Types: Number, Boolean
     - Operations: Add, Multiply, LessThan

--- test/simple_test.rb
+++ test/simple_test.rb
@@ -1,11 +1,43 @@
 require 'test_helper'
 
-class SimpleTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Simple::VERSION
-  end
+module Simple
+  class SimpleTest < Minitest::Test
+    def test_number
+      assert Number.new(2)
+      refute Number.new(2).reducible?
+      assert_equal Number.new(2).inspect, "«2»"
+    end
 
-  def test_it_does_something_useful
-    assert false
+    def test_add
+      assert expr = Add.new(Number.new(3), Number.new(4))
+      assert expr.reducible?
+      assert_equal Number.new(7), expr.reduce
+      assert_equal "«3 + 4»", expr.inspect
+    end
+
+    def test_multiply
+      assert expr = Multiply.new(Number.new(3), Number.new(4))
+      assert expr.reducible?
+      assert_equal Number.new(12), expr.reduce
+      assert_equal "«3 * 4»", expr.inspect
+    end
+
+    def test_boolead
+      assert Boolean.new(true)
+      assert Boolean.new(false)
+
+      refute Boolean.new(true).reducible?
+      refute Boolean.new(false).reducible?
+
+      assert_equal Boolean.new(true).inspect, "«true»"
+      assert_equal Boolean.new(false).inspect, "«false»"
+    end
+
+    def test_less_than
+      assert expr = LessThan.new(Number.new(3), Number.new(4))
+      assert expr.reducible?
+      assert_equal Boolean.new(true), expr.reduce
+      assert_equal "«3 < 4»", expr.inspect
+    end
   end
 end
